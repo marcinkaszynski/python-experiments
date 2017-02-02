17ae327cd2a3d18cf31b2a5e2306138f9c2dfdb5
Marcin Kaszynski <marcin.kaszynski@gmail.com>
2017-02-02 12:21:02 +0100

    Make test more readable.

--- background-processes/background/tests.py
+++ background-processes/background/tests.py
@@ -6,16 +6,25 @@ import time
 
 class TestBackgroundProcesses(unittest.TestCase):
     def test_basic(self):
-        self.assertRaises(requests.ConnectionError,
-                          requests.get, 'http://localhost:30001')
-        with bg.shell_cmds(['python -m http.server 30001']):
+        def server_online_at(port):
+            try:
+                resp = requests.get('http://localhost:30001')
+                return resp.status_code == 200
+            except requests.ConnectionError:
+                return False
+
+        self.assertFalse(server_online_at(30001))
+        self.assertFalse(server_online_at(30002))
+
+        with bg.shell_cmds(['python3 -m http.server 30001',
+                            'python3 -m http.server 30002']):
             # TODO: wait by checking availability, not a blind delay
             time.sleep(1)
-            resp = requests.get('http://localhost:30001')
-            self.assertEqual(200, resp.status_code)
+            self.assertTrue(server_online_at(30001))
+            self.assertTrue(server_online_at(30002))
 
-        self.assertRaises(requests.ConnectionError,
-                          requests.get, 'http://localhost:30001')
+        self.assertFalse(server_online_at(30001))
+        self.assertFalse(server_online_at(30002))
 
 
 if __name__ == '__main__':
